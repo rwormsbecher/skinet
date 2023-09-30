f4be3182cbad2297c6fcd6783c880de74dee56ce
rwormsbnecher <rodney22@live.nl>
2023-09-30 13:17:35 +0200

    initial commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿
+using Core.Entities;
+using Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly StoreContext context;
+
+    public ProductsController(StoreContext context)
+    {
+        this.context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Product>>> GetProducts()
+    {
+        var products = await context.Products.ToListAsync();
+
+        return products;
+    }
+
+
+    [HttpGet("{id}")]
+
+    public async Task<ActionResult<Product>> GetProducts(int id)
+    {
+        return await context.Products.FindAsync(id);
+    }
+}
