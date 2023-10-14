ade7cc75d831e2db3dc4738c52a86a0056929a29
rwormsbnecher <rodney22@live.nl>
2023-10-14 13:57:38 +0200

    add identity classes

---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace API;
+
+public static class IdentityServiceExtensions
+{
+    public static IServiceCollection AddIDentityServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppIdentityDbContext>(opt =>
+        {
+            opt.UseSqlite(configuration.GetConnectionString("IdentityConnection"));
+        });
+
+        return services;
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -12,6 +12,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIDentityServices(builder.Configuration);
 
 var app = builder.Build();
 
